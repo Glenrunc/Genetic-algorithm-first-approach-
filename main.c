7bc14a6c2c1193e43ee4ca94011c0066db9b3263
Glenunciter <pourcinematteo@gmail.com>
2022-11-28 14:41:44 +0100

    Main

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
 #include "Population.h"
-
 int main(){
     //Définition des différentes données d'une population
     srand(time(NULL));
-    int taille_population = rand()%180+20;
-    int taux_selection = (rand()%90+10 *taille_population)/100;
-    int nombre_generation = rand()%180+20;
+    int taille_population = 300;
+    int taux_selection = 290;
+    int nombre_generation = 3;
 
     popu P1 = NULL;
     P1 = initialize_population(P1,taille_population,taille_individu);
-    //debut
-
-    P1 = nGen(P1,taille_population,taille_individu,probabilite_croisement,nombre_generation);
-    quick_sort_population(P1,trouver_queux(P1));
-    tSelect(P1,taux_selection,taille_population);
-
+    P1 = nGen(P1,taille_population,taille_individu,probabilite_croisement,taux_selection,nombre_generation);
     affichage_population(P1);
-
     free_population(P1);
    
     return 0;
