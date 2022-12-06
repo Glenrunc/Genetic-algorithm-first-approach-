099af0137211495573bae4c49503a05f5dba6822
Glenunciter <pourcinematteo@gmail.com>
2022-12-06 15:56:16 +0100

    dernier commit

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 main: Population.o individu.o main.o
-	gcc -o main.exe individu.o Population.o main.o -lm
-Population.o: Population.c
-	gcc -o Population.o -c Population.c -W -Wall -pedantic
-individu.o: individu.c
-	gcc -o individu.o -c individu.c -W -Wall -pedantic
-main.o: main.c Population.h individu.h
+	gcc -o main.exe individu.o Population.o main.o -lm 
+Population.o: Population.c Population.h
+	gcc -o Population.o -c Population.c -W -Wall -pedantic 
+individu.o: individu.c individu.h
+	gcc -o individu.o -c individu.c -W -Wall -pedantic 
+main.o: main.c
 	gcc -o main.o -c main.c -W -Wall -pedantic 
 clean:
 	rm -rf *.o *.exe
--- main.c
+++ main.c
@@ -8,9 +8,9 @@ int main()
     int taille_population, taux_selection, nombre_generation,n = -1;
 
     //récupération depuis l'utilisateur
-    get_integer(&taille_population,10,200,"population");
+    get_integer(&taille_population,20,200,"population");
     get_integer(&taux_selection,10,90,"taux de selection");
-    get_integer(&nombre_generation,10,200,"generation");
+    get_integer(&nombre_generation,20,200,"generation");
     get_integer(&n,0,1,"affichage");
 
     //Lancement de la simulation
@@ -20,6 +20,9 @@ int main()
 
     //affichage du meilleur individu
     quick_sort_population(P1,trouver_queue(P1));
+    if(n == 1){
+        printf("Taille Population : %d ~ Taux de selection : %d   ~ Nombre de generations : %d \n ",taille_population,taux_selection,nombre_generation);
+    }
     printf("\n MEILLEUR INDIVIDU ~qualite (%.3f)~  ~decodage (%d)~ :  ",qualite_individu_f2(P1->indivPopu),valeur_base_2_to_base_10(P1->indivPopu));
     affichage_individu(P1->indivPopu.indiv);
     printf("\n");
